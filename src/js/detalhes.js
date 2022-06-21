d6a93b5266bebacb75355247c0e1d38e38433d41
dolabelag <104024845+dolabelag@users.noreply.github.com>
2022-06-21 15:20:22 -0300

    Update detalhes.js

--- src/js/detalhes.js
+++ src/js/detalhes.js
@@ -1,4 +1,4 @@
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=d59f62f637869af684bdb3c00525956a';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
